5e624e6bc3ab578bf46843f2db49b9bf618498a7
juned <1132210708@mitwpu.edu.in>
2023-05-31 22:44:18 +0530

    ;

---
+++ Day_3.py
@@ -0,0 +1,10 @@
+# Dictionaries
+# A dictionary in Python is a collection of key-value pairs.
+
+state_capitals = {
+    'Arkansas': 'Little Rock',
+    'Colorado': 'Denver',
+    'California': 'Sacramento',
+    'Georgia': 'Atlanta'
+}
+
