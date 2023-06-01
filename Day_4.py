a2b7bfdc1b24f3b3f2e80e5f79b8e40ce7df63cf
juned <1132210708@mitwpu.edu.in>
2023-06-01 23:24:36 +0530

    day 4 remaining files.

--- Day_4.py
+++ Day_4.py
@@ -1,3 +1,5 @@
+from array import *
+from enum import Enum
 import datetime
 # Built in Modules and Functions
 # Built_ins=[
@@ -181,3 +183,104 @@ print(repr(today))
 # $ pip install[package_name] == x.x.x  # specific version of the package
 # $ pip install '[package_name]>=x.x.x'  # minimum version of the package
 # where x.x.x is the version number of the package you want to install.
+
+
+def hello():
+ """Say hello to your friends."""
+ print("Hello my friends!")
+
+
+hello()
+
+
+def hello(name, language="en"):
+ """Say hello to a person.
+ Arguments:
+ name: the name of the person
+ language: the language in which the person should be greeted
+ """
+ print(name)
+
+
+hello("tejashri")
+
+
+class Color(Enum):
+ red = 1
+ green = 2
+ blue = 3
+
+
+print(Color.red)  # Color.red
+print(Color(1))  # Color.red
+print(Color['red'])  # Color.red'
+
+
+a = {1, 2, 3, 4}
+b = {1, 2, 3, 56, 65}
+print(a.intersection(b))
+print(a.union(b))
+print(a.difference(b))
+print(a.symmetric_difference(b))
+print(a.issuperset(b))
+print(a.isdisjoint(b))
+
+print(1 in a)
+
+restaurants = ["McDonald's", "Burger King", "McDonald's", "Chicken Chicken"]
+unique_restaurants = set(restaurants)
+print(unique_restaurants)
+
+
+x = 'Hi'
+
+
+def read_x():
+ print(x)  # x is just referenced, therefore assumed global
+
+
+read_x()  # prints Hi
+y = 4.5
+
+
+def read_y():
+ print(y)  # here y is just referenced, therefore assumed global
+
+
+read_y()
+
+
+x = 5
+print(x)  # out: 5
+del x
+print(x)
+
+
+x = 5
+print(x)  # out: 5
+del x
+print(x)
+
+number = 5
+if number > 2:
+ print("Number is bigger than 2.")
+elif number < 2:  # Optional clause (you can have multiple elifs)
+ print("Number is smaller than 2.")
+else:  # Optional clause (you can only have one else)
+ print("Number is 2.")
+
+a = 10
+while True:
+ a = a-1
+ print(a)
+ if a < 7:
+   break
+print('Done.')
+
+
+my_array = array('i', [1, 2, 3, 4, 5])
+print(my_array[1])
+# 2
+print(my_array[2])
+# 3
+print(my_array[0])
